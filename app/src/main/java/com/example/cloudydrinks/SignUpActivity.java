3a97e40c8877aa62fb1c51b01d757bddfc0589af
cowd90 <tienn.dungg.980@gmail.com>
2023-10-03 07:51:15 +0700

    OPT sending

--- app/src/main/java/com/example/cloudydrinks/SignUpActivity.java
+++ app/src/main/java/com/example/cloudydrinks/SignUpActivity.java
@@ -1,19 +1,39 @@
 package com.example.cloudydrinks;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private TextView tv_signIn;
     private Toolbar toolbar;
+    private Button btn_nextStep;
+    private EditText mobileInput;
+    private CheckBox policyInput;
+    String phoneNumber;
+    boolean isTick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +41,14 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         tv_signIn = findViewById(R.id.sign_in);
+        btn_nextStep = findViewById(R.id.btn_next_verify_phone_number);
+        mobileInput = findViewById(R.id.et_mobile_input);
+        policyInput = findViewById(R.id.cb_policy);
+
+        // set up toolbar of sign up activity
         toolbar = findViewById(R.id.sign_up_toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,11 +58,31 @@ public class SignUpActivity extends AppCompatActivity {
 
 
         tv_signIn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
             }
         });
+
+        // Check if phone number input, terms of service and policies is filled
+
+        btn_nextStep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SignUpActivity.this, SignUpVerificationActivity.class);
+                i.putExtra("mobile", mobileInput.getText().toString());
+                startActivity(i);
+            }
+        });
     }
+   private CompoundButton.OnCheckedChangeListener checkedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+       @Override
+       public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+           if (isChecked) {
+               
+           }
+       }
+   };
 }
---
+++ app/src/main/java/com/example/cloudydrinks/SignUpVerificationActivity.java
@@ -0,0 +1,178 @@
+package com.example.cloudydrinks;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextWatcher;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SignUpVerificationActivity extends AppCompatActivity {
+    private EditText inputCode1, inputCode2, inputCode3, inputCode4, inputCode5, inputCode6;
+    private Toolbar toolbar;
+    private TextView notification;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_verification);
+
+        // set up toolbar of verification activity
+        toolbar = findViewById(R.id.verification_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        // set up otp input
+        inputCode1 = findViewById(R.id.inputCode1);
+        inputCode1.requestFocus();
+        inputCode2 = findViewById(R.id.inputCode2);
+        inputCode3 = findViewById(R.id.inputCode3);
+        inputCode4 = findViewById(R.id.inputCode4);
+        inputCode5 = findViewById(R.id.inputCode5);
+        inputCode6 = findViewById(R.id.inputCode6);
+        setupOTPInputs();
+
+        notification = findViewById(R.id.tv_notification);
+        String msg = "Mã xác thực (OTP) đã được gửi qua Tin nhắn của số";
+        SpannableString ss = new SpannableString(msg);
+        StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
+        ss.setSpan(boldSpan, 34, 42, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        notification.setText(ss);
+    }
+
+    private void setupOTPInputs() {
+        inputCode1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().trim().isEmpty()) {
+                    inputCode2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().trim().isEmpty()) {
+                    inputCode3.requestFocus();
+                } else {
+                    inputCode1.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().trim().isEmpty()) {
+                    inputCode4.requestFocus();
+                } else {
+                    inputCode2.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().trim().isEmpty()) {
+                    inputCode5.requestFocus();
+                } else {
+                    inputCode3.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        inputCode5.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(!s.toString().trim().isEmpty()) {
+                    inputCode6.requestFocus();
+                } else {
+                    inputCode4.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        inputCode6.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if(s.toString().trim().isEmpty()) {
+                    inputCode5.requestFocus();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+}
